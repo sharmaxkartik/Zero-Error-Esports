c524bd952316613db93f33b428691116a790ec85
Kartik Sharma <kartikgwl13@gmail.com>
2025-05-15 22:25:39 +0530

    added images in gamecon page

--- app/events/gameocon/page.tsx
+++ app/events/gameocon/page.tsx
@@ -25,7 +25,7 @@ const gameOConEvents = [
     date: "May 19-20-21, 2025",
     rewards: "Tickets to IGDC 25",
     participants: "50 Teams",
-    image: "/placeholder.svg?height=300&width=500",
+    image: "/images/gamejam.jpg?height=300&width=500",
     category: "Game Jam",
     description:
       "A 3-day challenge to build a game from scratch. Create. Collaborate. Compete.",
@@ -67,7 +67,7 @@ const gameOConEvents = [
     date: "May 20-24, 2025",
     location: "ITM Gwalior",
     participants: "60 Teams",
-    image: "/placeholder.svg?height=300&width=500",
+    image: "/images/freefire.webp?height=300&width=500",
     category: "Tournament",
     description:
       "Ready to prove your squad is the best in India? Step into Gwalior's biggest-ever Free Fire MAX tournament and compete with 60 elite teams in a heart-pounding survival showdown for glory, fame, and bragging rights.",
@@ -87,7 +87,7 @@ const gameOConEvents = [
     date: "May 20-24, 2025",
     location: "ITM Gwalior",
     participants: "50 Teams",
-    image: "/placeholder.svg?height=300&width=500",
+    image: "/images/valorantgame.jpeg?height=300&width=500",
     category: "Tournament",
     description: "Gear up for Gwalior's most electrifying Valorant tournament!",
     coordinator: "Tournament Director",
@@ -106,7 +106,7 @@ const gameOConEvents = [
     date: "May 20-24, 2025",
     location: "ITM Gwalior",
     participants: "100 Players",
-    image: "/placeholder.svg?height=300&width=500",
+    image: "/images/tekken8.jpg?height=300&width=500",
     category: "Tournament",
     description:
       "Think you've got what it takes to be India's best Tekken fighter? Step into the ring and battle your way to glory at the Tekken 8 Tournament in Gwalior!",
@@ -126,7 +126,7 @@ const gameOConEvents = [
     date: "May 20-24, 2025",
     location: "ITM Gwalior",
     participants: "100 Players",
-    image: "/placeholder.svg?height=300&width=500",
+    image: "/images/fc25.webp?height=300&width=500",
     category: "Tournament",
     description:
       "Get ready for an action-packed FIFA showdown! From intense knockout rounds to a live LAN finale in Gwalior, this is your chance to compete on India's biggest FIFA stage.",
